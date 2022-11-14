2bc92dc48c0570ee73b391a4ff76304dae48ef43
korama9320 <3bdo9320@gmail.com>
2022-11-14 15:24:46 +0200

    paypal

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const express = require('express');
-const app = express()
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const app = express();
 // const app = express.Router()
 const port = 8000;
 const socketPort = 4000;
-const mongo = require('./config/mongoose');
-const User = require('./models/User');
-const cors = require('cors');
-const Job = require('./models/Order');
-const res = require('express/lib/response');
-const userRouter = require('./routes/userRoute');
-const exerciseRouter = require('./routes/exerciseRoute');
-const productsRouter = require('./routes/productRoute');
-const healthyFoodRouter = require('./routes/Health-System');
+const mongo = require("./config/mongoose");
+const User = require("./models/User");
+const cors = require("cors");
+const Job = require("./models/Order");
+const res = require("express/lib/response");
+const userRouter = require("./routes/userRoute");
+const exerciseRouter = require("./routes/exerciseRoute");
+const productsRouter = require("./routes/productRoute");
+const healthyFoodRouter = require("./routes/Health-System");
 const notificationRouter = require("./routes/notificationRoute");
 const orderRouter = require("./routes/orderRoute");
+const engines = require("consolidate");
 
-app.use(function (req, res, next)
-{
+app.use(function (req, res, next) {
   res.set({
-    'Access-Control-Expose-Headers': '*',
-  })
+    "Access-Control-Expose-Headers": "*",
+  });
   next();
-})
+});
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
@@ -219,20 +219,21 @@ const io = require("socket.io")(http, {
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/exercises', exerciseRouter);
-app.use('/api/v1/products', productsRouter);
-app.use('/api/v1/healthyfoods', healthyFoodRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/exercises", exerciseRouter);
+app.use("/api/v1/products", productsRouter);
+app.use("/api/v1/healthyfoods", healthyFoodRouter);
 app.use("/api/v1/Orders", orderRouter);
 app.use("/api/v1/notification", notificationRouter);
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/public", express.static("public"));
 app.use("/public/exercises", express.static("public/exercises"));
 app.use("/public/HealthyFood", express.static("public/HealthyFood"));
 app.use("/public/profile", express.static("public/profile"));
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.engine("ejs", engines.ejs);
+app.set("views", "./Views");
+app.set("view engine", "ejs");
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 http.listen(socketPort, () =>
   console.log(`Example Socket listening on port ${socketPort}!`)
 );
